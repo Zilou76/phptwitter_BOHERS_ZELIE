1215aeaa51b30030673f1276588d418c1dc6e2b3
Zilou76 <122106683+Zilou76@users.noreply.github.com>
2023-03-12 11:57:26 +0100

    Update rechercher.php

--- aatwitter/rechercher.php
+++ aatwitter/rechercher.php
@@ -27,6 +27,12 @@ if($_GET['recherche']){
     $requete->execute($data);
 
     $articles = $requete->fetchAll(PDO::FETCH_ASSOC);
+
+    
+$requete2=$database->prepare('SELECT * FROM users LIMIT 1');
+$requete2->execute();
+$users=$requete2 -> fetchAll(PDO::FETCH_ASSOC);
+
 //var_dump($articles);
 
     if($articles == null){
@@ -34,9 +40,18 @@ if($_GET['recherche']){
     }else{
         foreach($articles as $article){?>
             <div class="boite">
-                <p><?php echo $article['message'];?></p>
-                <p><?php echo $article['date'];?></p>
+        
+                <div>
+                <p><?php echo $users[0]['pseudo']?></p>
+                <img src="<?php echo $users[0]['image'];?>" alt="" >
+                </div>
+                <p> <?php echo $article['message'];?></p>
+                <span ><?php echo $article['date'];?></span>
+                <div>
+                <a href="hash/supprimer.php?id=<?php echo $article['id']; ?>">Supprimer</a>
+                </div>
             </div>
+
     <?php
         }
     }
